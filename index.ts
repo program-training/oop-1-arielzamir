e130c8f7e3566eef3213d14e8a54703dd950a7cc
Ariel Zamir <arielzamir100@gmail.com>
2023-08-27 13:35:53 +0300

    Added section 1.

--- index.ts
+++ index.ts
@@ -1,6 +1,21 @@
 // את הקוד שלכם תכתבו כאן
 
 //  1. Rectangle - מלבן
+// class Rectangle {
+//   height: number;
+//   width: number;
+
+//   constructor(height: number, width: number) {
+//     this.height = height;
+//     this.width = width;
+//   }
+
+//   area() {
+//     return this.height * this.width;
+//   }
+// }
+// const rectangle = new Rectangle(5, 10);
+// console.log(rectangle.area());
 
 //  2. Square - ריבוע
 
